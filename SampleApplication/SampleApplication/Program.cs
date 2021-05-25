7b4605e8d70fac7f199d1fbbba8e59c82a9ee3c8
gokul59 <57764228+gokul59@users.noreply.github.com>
2021-05-25 12:02:37 +0000

    Add files via upload

---
+++ SampleApplication/SampleApplication/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SampleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Printing Values from 1 to 11");
+            for (int i=0; i<=11; i++)
+            {
+                Console.WriteLine("Printing ", i);
+            }
+        }
+    }
+}
